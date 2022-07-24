5e35fa3fe9cbf6edfe2610823c548af9dca78e74
mmmiiiuuu <109843520+mmmiiiuuu@users.noreply.github.com>
2022-07-23 18:53:45 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1200, 1200);
+  createCanvas(17, 106);
 
   // Create and label the tiles
   tiles[0] = new Tile(tileImages[0], ["AAA", "AAA", "AAA", "AAA"]);
